94a766ff0df0b2e17561c5694b1f01ddf862ff93
eltzc <vasechkin.lelya@inbox.ru>
2025-11-12 22:52:09 +0500

    Add DataBase

--- data_app/admin.py
+++ data_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import StudentData # Импортируем вашу модель
+
+# Регистрируем модель StudentData в админке
+admin.site.register(StudentData)
